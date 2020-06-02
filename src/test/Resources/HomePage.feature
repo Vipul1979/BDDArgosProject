619cdcc31a390e929181a083f6a9aa9fc34d114d
Vipul1979 <bhesaniav@gmail.com>
2020-06-02 15:43:35 +0100

    first commit

--- src/test/Resources/HomePage.feature
+++ src/test/Resources/HomePage.feature
@@ -1,7 +1,6 @@
 Feature: Argos website homepage functionality
-  @smoke
+
   Scenario: Argos HomePage
     Given user is on argos homepage
     When user search puma
-    And user click on search button
     Then user can see all puma product
--- src/test/java/BDDArgosProject/RunCukesTest.java
+++ src/test/java/BDDArgosProject/RunCukesTest.java
@@ -6,8 +6,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\Resources\\HomePage.feature",
-        tags = {"@smoke"})
+@CucumberOptions(features = "src/test/Resources/HomePage.feature")
 
 public class RunCukesTest {
 }
