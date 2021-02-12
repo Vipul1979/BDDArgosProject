bf07e658194177258be883030e44910598c407b5
Vipul1979 <bhesaniav@gmail.com>
2021-02-12 21:30:24 +0000

    vipul.feature is up to date

--- src/test/java/BDDArgosProject/RunCukesTest.java
+++ src/test/java/BDDArgosProject/RunCukesTest.java
@@ -6,10 +6,10 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/Resources/ResultPage.feature")
-/* @CucumberOptions(features = "src/test/Resources/HomePage.feature",
-plugin = {"html:target/cucumber-html-report",
-"json:target/cucumber-json-report.json"})*/
+//@CucumberOptions(features = "src/test/Resources/ResultPage.feature")
+ @CucumberOptions(features = "src/test/Resources/HomePage.feature")
+//plugin = {"html:target/cucumber-html-report",
+//"json:target/cucumber-json-report.json"})*/
 //this is vipul.feature branch
 
 public class RunCukesTest {
